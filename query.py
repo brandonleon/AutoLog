cab3fbba6786ab353e5e01f6bedb6542f5ade6b2
Brandon Leon <brandonleon@pm.me>
2023-10-24 15:19:48 -0700

    feat: Enhance Data Presentation with Rich Library
    In this commit, we've enhanced the data presentation in the `query_logs` function by using the Rich library to create more visually appealing and structured tables. The query now includes additional vehicle details such as Year, Make, Model, and trim by performing a LEFT JOIN with the `vehicles` table. This change makes the log entries more informative and easier to read.
    
    Additionally, we've improved the layout and formatting of the displayed data. The tables now provide a clear and organized view of log and vehicle information.

--- query.py
+++ query.py
@@ -1,5 +1,7 @@
 import sqlite3
 import typer
+from rich.console import Console
+from rich.table import Table
 from constants import DB_FILE
 
 
@@ -11,24 +13,34 @@ def query_logs(page: int = 1, page_size: int = 10, vehicle_id: str = None):
         offset = (page - 1) * page_size
 
         # Prepare the SQL query with parameterized query
-        query = "SELECT EntryDate, EntryTime, EntryType, Services FROM logs"
+        query = """
+            SELECT v.Year, v.Make, v.Model, v.trim, l.EntryDate, l.EntryTime,
+            l.EntryType, l.Services
+            FROM logs l
+            LEFT JOIN vehicles v ON l.VehicleID = v.id
+        """
+
         params = ()
         if vehicle_id:
-            query += " WHERE VehicleID = ?"
+            query += " WHERE l.VehicleID = ?"
             params = (vehicle_id,)
 
-        query += " ORDER BY EntryDate, EntryTime LIMIT ? OFFSET ?"
+        query += " ORDER BY l.EntryDate DESC, l.EntryTime DESC LIMIT ? OFFSET ?"
         params += (page_size, (page - 1) * page_size)
 
         cursor.execute(query, params)
         if logs := cursor.fetchall():
-            typer.echo(f"Page {page}:")
+            print(f"Page {page}:")
+            console = Console()
+            table = Table(
+                "Vehicle", "EntryDate", "EntryTime", "EntryType", "Services"
+            )
             for log in logs:
-                # Format the log data as needed
-                formatted_log = f"EntryDate: {log[0]}, EntryTime: {log[1]}, EntryType: {log[2]}"
-                if Services := log[3]:
-                    formatted_log += f", Services: {Services}"
-                typer.echo(formatted_log)
+
+                table.add_row(f'{log[0]} {log[1]} {log[2]} {log[3]}', log[4], log[5], log[6], log[7])
+
+            console.print(table)
+            # Format the log data as needed
 
         else:
             typer.echo("No logs found on this page.")
@@ -39,7 +51,7 @@ def query_vehicles(page: int = 1, page_size: int = 10, vehicle_id: str = None):
         cursor = conn.cursor()
 
         # Calculate the OFFSET based on the page number and page size
-        query = "SELECT id, Make, Model, Year FROM vehicles"
+        query = "SELECT id, Make, Model, Year, mileage FROM vehicles"
         params = ()
         if vehicle_id:
             query += " WHERE id = ?"
@@ -53,7 +65,10 @@ def query_vehicles(page: int = 1, page_size: int = 10, vehicle_id: str = None):
             typer.echo(f"Page {page}:")
             for vehicle in vehicles:
                 # Format the vehicle data as needed
-                formatted_vehicle = f"id: {vehicle[0]}, Make: {vehicle[1]}, Model: {vehicle[2]}, Year: {vehicle[3]}"
+                formatted_vehicle = (
+                    f"id: {vehicle[0]}, Make: {vehicle[1]}, Model: {vehicle[2]}, "
+                    f"Year: {vehicle[3]}, Miles: {vehicle[4]}"
+                )
                 typer.echo(formatted_vehicle)
         else:
             typer.echo("No vehicles found on this page.")
