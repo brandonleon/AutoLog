f3598b8e418d0754c57b303bdb523bd26874c8e3
Brandon Leon <brandonleon@pm.me>
2023-10-24 15:18:29 -0700

    help text changes.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = typer.Typer()
 @app.command()
 def display_logs(page: int = 1, page_size: int = 10, vehicle_id: str = None):
     """
-    View logs from the database with optional filtering and pagination.
+    View logs with optional filtering and pagination.
     """
     query_logs(page, page_size, vehicle_id)
 
@@ -15,10 +15,10 @@ def display_logs(page: int = 1, page_size: int = 10, vehicle_id: str = None):
 @app.command()
 def display_vehicles(page: int = 1, page_size: int = 10, vehicle_id: str = None):
     """
-    View vehicles from the database with optional filtering and pagination.
+    View vehicles with optional filtering and pagination.
     """
     query_vehicles(page, page_size, vehicle_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app()
