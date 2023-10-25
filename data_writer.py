0235bc0faaf2a5085de53c0d4493a0541c91ff49
Brandon Leon <brandonleon@pm.me>
2023-10-24 20:24:14 -0700

    Refactor data_writer.py and improve code readability
    - Refactored data_writer.py to improve code readability and consistency.
    - Reorganized the 'add_record' function parameters for better alignment.
    - Enhanced the 'add_record' function to handle service categorization based on 'service' parameter.
    - Updated the cursor.execute() call to correctly pass the function parameters.
    - Added comments for clarity and documentation.

--- data_writer.py
+++ data_writer.py
@@ -4,27 +4,26 @@ This module contains the functions to write from the database.
 
 import sqlite3
 import typer
-from rich.console import Console
-from rich.table import Table
 from constants import DB_FILE
 from uuid import uuid4
 
 
-def add_record(vehicle_id: str,
-                  odometer: float,
-                  service: str,
-                  entry_date: str,
-                  entry_time: str,
-                  location: str,
-                  total_cost: float,
-                  ):
+def add_record(
+    vehicle_id: str,
+    odometer: float,
+    service: str,
+    entry_date: str,
+    entry_time: str,
+    location: str,
+    total_cost: float,
+):
     with sqlite3.connect(DB_FILE) as conn:
         cursor = conn.cursor()
         query = """
             INSERT INTO logs (ID, VehicleID, OdometerReading, EntryDate, EntryTime, Location, TotalCost, Services)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         """
-        match Services:
+        match service:
             case "Gas":
                 EntryType = "Gas"
             case "Air Filter":
@@ -51,6 +50,18 @@ def add_record(vehicle_id: str,
             case _:
                 EntryType = "Other"
 
-        cursor.execute(query, (str(uuid4()), vehicle_id, odometer, entry_date, entry_time, entry location, total_cost, service))
+        cursor.execute(
+            query,
+            (
+                str(uuid4()),
+                vehicle_id,
+                odometer,
+                entry_date,
+                entry_time,
+                location,
+                total_cost,
+                service,
+            ),
+        )
         conn.commit()
         typer.echo(f"Added log entry for {vehicle_id}.")
