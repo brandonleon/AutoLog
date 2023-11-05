4599bb469feefc78f8dd0df89358f19d04a28932
Brandon Leon <brandonleon@pm.me>
2023-11-05 15:42:12 -0800

    Rename data_writer module and reorganize command functions
    This commit involves two significant changes:
    
    1. Renaming the `data_writer.py` module to `add_record.py`:
       The `data_writer.py` module, which was responsible for adding records to the database, has been renamed to `add_record.py`. This change better reflects the module's purpose, which is specifically for adding records to the database.
    
    2. Reorganizing command functions in `main.py`:
       The command functions for adding log entries have been moved to the new `add_record.py` module, resulting in a more organized project structure. This reorganization separates concerns, making it easier to locate and manage the functions related to adding records.
    
    With these changes, the project structure is improved, and the naming of modules aligns more closely with their respective functionalities. This enhances code clarity and maintainability within the project.

--- add_record.py
+++ add_record.py
@@ -1,14 +1,24 @@
 """
 This module contains the functions to write from the database.
 """
-
 import sqlite3
-import typer
-from constants import DB_FILE
+from datetime import datetime
+from enum import Enum
+from typing import Annotated
 from uuid import uuid4
 
+import typer
 
-def add_record(
+from constants import DB_FILE
+from database_utilities import initialize_database
+
+# Initialize the database (Create the file and tables if they don't exist).
+initialize_database()
+# Create the Typer app
+app = typer.Typer()
+
+
+def add_fuel_up(
     vehicle_id: str,
     odometer: float,
     service: str,
@@ -65,3 +75,40 @@ def add_record(
         )
         conn.commit()
         typer.echo(f"Added log entry for {vehicle_id}.")
+
+
+class ServiceTypes(str, Enum):
+    gas = "Gas"
+    air_filter = "Air Filter"
+    cabin_filter = "Cabin Filter"
+    oil_change = "Oil Change"
+    tire_rotation = "Tire Rotation"
+    tire_replacement = "Tire Replacement"
+    car_wash = "Car Wash"
+    car_detailing = "Car Detailing"
+
+
+@app.command()
+def fuel_up(
+    vehicle_id: Annotated[str, typer.Option(help="Vehicle ID")],
+    odometer: Annotated[float, typer.Option(help="Odometer reading")],
+    service: Annotated[ServiceTypes, typer.Option(help="Service performed")],
+    entry_date: Annotated[
+        str, typer.Option(help="Date of service")
+    ] = datetime.now().strftime("%m/%d/%Y"),
+    entry_time: Annotated[
+        str, typer.Option(help="Time of service")
+    ] = datetime.now().strftime("%I:%M %p"),
+    location: Annotated[str, typer.Option(help="Location of service")] = "Home",
+    total_cost: Annotated[float, typer.Option(help="Total cost of service")] = "0.0",
+):
+    """
+    Add a fuel up entry to the database.
+    """
+    add_fuel_up(
+        vehicle_id, odometer, service, entry_date, entry_time, location, total_cost
+    )
+
+
+if __name__ == "__main__":
+    app()
--- main.py
+++ main.py
@@ -8,47 +8,15 @@ from enum import Enum
 
 import typer
 import display
-from data_writer import add_record
+import add_record
 from database_utilities import initialize_database
 
 # Initialize the database (Create the file and tables if they don't exist).
 initialize_database()
 # Create the Typer app
 app = typer.Typer()
-app.add_typer(display.app, name="display")
-
-
-class ServiceTypes(str, Enum):
-    gas = "Gas"
-    air_filter = "Air Filter"
-    cabin_filter = "Cabin Filter"
-    oil_change = "Oil Change"
-    tire_rotation = "Tire Rotation"
-    tire_replacement = "Tire Replacement"
-    car_wash = "Car Wash"
-    car_detailing = "Car Detailing"
-
-
-@app.command()
-def add_log_entry(
-    vehicle_id: Annotated[str, typer.Option(help="Vehicle ID")],
-    odometer: Annotated[float, typer.Option(help="Odometer reading")],
-    service: Annotated[ServiceTypes, typer.Option(help="Service performed")],
-    entry_date: Annotated[
-        str, typer.Option(help="Date of service")
-    ] = datetime.now().strftime("%m/%d/%Y"),
-    entry_time: Annotated[
-        str, typer.Option(help="Time of service")
-    ] = datetime.now().strftime("%I:%M %p"),
-    location: Annotated[str, typer.Option(help="Location of service")] = "Home",
-    total_cost: Annotated[float, typer.Option(help="Total cost of service")] = "0.0",
-):
-    """
-    Add a log entry for a vehicle.
-    """
-    add_record(
-        vehicle_id, odometer, service, entry_date, entry_time, location, total_cost
-    )
+app.add_typer(display.app, name="display", help="Display records from the database.")
+app.add_typer(add_record.app, name="add", help="Add records to the database.")
 
 
 if __name__ == "__main__":
